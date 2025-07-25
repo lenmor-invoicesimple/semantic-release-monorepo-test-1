f53bea8fa892b7f6da9696435d423a0a465cd53a
Lenmor Dimanalata <lenmor@invoicesimple.com>
2025-07-25 14:15:35 -0400

    feat: change is-odd

--- packages/demo-is-odd/lib/demo-is-odd.ts
+++ packages/demo-is-odd/lib/demo-is-odd.ts
@@ -2,5 +2,6 @@ export function isOdd(num: number): boolean {
   console.log("isOdd called with:", num);
   console.log("isOdd called with222:", num);
   console.log("isOdd called with333:", num);
+  console.log("isOdd called with444:", num);
   return num % 2 !== 0;
 }
